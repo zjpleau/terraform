b73f2cc53dc6d1f9606cf4cd8f3a99041264d81f
Zachary Pleau <zachary@mac102.local>
2025-05-13 11:10:04 -0700

    fix: remove unsupported use_lockfile from backend

--- backend.tf
+++ backend.tf
@@ -1,18 +1,14 @@
 terraform {
   backend "s3" {
-    bucket         = "my-tf-state-homelabs-dev"
-    key            = "homelabs-dev/terraform.tfstate"
-    region         = "us-east-1"
+    bucket  = "my-tf-state-homelabs-dev"
+    key     = "homelabs-dev/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
 
-    # enable the new locking mechanism (replaces the old `dynamodb_table`‑only workflow)
-    use_lockfile   = true
-
-    # still specify your DynamoDB table so Terraform will write locks there
+    # state‐locking in DDB
     dynamodb_table = "my-tf-state-locks-homelabs-dev"
 
-    encrypt        = true
-
-    # make sure this matches exactly the [profile homelabs-dev] stanza in ~/.aws/config
-    profile        = "homelabs-dev"
+    # which AWS profile to use
+    profile = "homelabs-dev"
   }
 }
