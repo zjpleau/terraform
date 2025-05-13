5b26910071229a0f9f3dcee6120c4e28ecc68652
Zachary Pleau <zachary@mac102.local>
2025-05-13 08:58:52 -0700

    chore: add simple null_resource for Atlantis test

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "null" {}
+
+resource "null_resource" "example" {
+  triggers = {
+    timestamp = timestamp()
+  }
+}
