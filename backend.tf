23324d20badbfa9691ec2d121e222a0d4c4d895b
Zachary Pleau <zachary@mac102.local>
2025-05-13 11:07:02 -0700

    chore: configure Terraform remote‑state backend (S3 + DynamoDB)

--- backend.tf
+++ backend.tf
@@ -3,7 +3,16 @@ terraform {
     bucket         = "my-tf-state-homelabs-dev"
     key            = "homelabs-dev/terraform.tfstate"
     region         = "us-east-1"
+
+    # enable the new locking mechanism (replaces the old `dynamodb_table`‑only workflow)
+    use_lockfile   = true
+
+    # still specify your DynamoDB table so Terraform will write locks there
     dynamodb_table = "my-tf-state-locks-homelabs-dev"
+
     encrypt        = true
+
+    # make sure this matches exactly the [profile homelabs-dev] stanza in ~/.aws/config
+    profile        = "homelabs-dev"
   }
 }
