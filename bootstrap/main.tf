4bd286a383db824ae2bd25c75fad4e92ab7ac94d
Zachary Pleau <zachary@mac102.local>
2025-05-14 18:39:23 -0700

    chore: add dev remote‐state bootstrap (S3 + DynamoDB)

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,15 +1,5 @@
 terraform {
   required_version = ">= 1.0"
-
-  backend "s3" {
-    # these will get overridden by -backend-config on init:
-    bucket         = var.state_bucket_name
-    key            = "bootstrap/terraform.tfstate"
-    region         = var.aws_region
-    dynamodb_table = var.lock_table_name
-    encrypt        = true
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
