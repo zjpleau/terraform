cadb9207342237cc0ccf59d337dff17c6d45b7ff
Zachary Pleau <zachary@mac102.local>
2025-05-14 17:51:43 -0700

    chore: remove old root TF configs and bootstrap dirs; ignore local .terraform and lock files

--- bootstrap/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "state_bucket" {
-  description = "Name of the S3 bucket for Terraform state"
-  value       = aws_s3_bucket.tfstate.bucket
-}
-
-output "lock_table" {
-  description = "Name of the DynamoDB table for state locking"
-  value       = aws_dynamodb_table.lock.name
-}
